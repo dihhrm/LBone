aab01b5572b7a2bf214f6f4d87e929965c50e6bc
Diego <diego_rodrigues94@outlook.com>
2017-05-14 22:46:12 -0300

    Patch to fix error on migration

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ContentApp::Application.initialize!
+
+require File.expand_path('../../lib/patches/abstract_mysql2_adapter', __FILE__)
